f576fefe1eb5aeb35c62c6d4a8566671fe9da7c9
Елизавета Бельская <nefrf@mail.ru>
2022-05-09 13:57:35 +0300

    Tests Added

---
+++ src/ru/kpfu/itis/TestGenerator.java
@@ -0,0 +1,47 @@
+package ru.kpfu.itis;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+import org.uncommons.maths.random.*;
+
+public class TestGenerator {
+    public void generateUniformlyDistributedTest(String path, int numberOfElements) {
+        try (FileWriter file = new FileWriter(path)) {
+            file.write(numberOfElements + "\n");
+            Random random = new Random();
+            for (int i = 0; i < numberOfElements; i++) {
+                int elem = random.nextInt();
+                file.write(elem + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void generateGaussianTest(String path, int numberOfElements) {
+        try (FileWriter file = new FileWriter(path)) {
+            file.write(numberOfElements + "\n");
+            Random random = new Random();
+            for (int i = 0; i < numberOfElements; i++) {
+                int elem = (int) (random.nextGaussian()*Integer.MAX_VALUE);
+                file.write(elem + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void generateExponentialTest(String path, int numberOfElements) {
+        try (FileWriter file = new FileWriter(path)) {
+            file.write(numberOfElements + "\n");
+            Random random = new Random();
+            for (int i = 0; i < numberOfElements; i++) {
+                int elem = (int) ((new ExponentialGenerator(10, random)).nextValue() * 100);
+                file.write(elem + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
