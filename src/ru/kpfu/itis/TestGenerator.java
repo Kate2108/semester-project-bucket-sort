c01dd941328b2b74b272810accf10ab67e8c61d1
Елизавета Бельская <nefrf@mail.ru>
2022-05-09 15:33:35 +0300

    New tests Added

--- src/ru/kpfu/itis/TestGenerator.java
+++ src/ru/kpfu/itis/TestGenerator.java
@@ -9,9 +9,8 @@ public class TestGenerator {
     public void generateUniformlyDistributedTest(String path, int numberOfElements) {
         try (FileWriter file = new FileWriter(path)) {
             file.write(numberOfElements + "\n");
-            Random random = new Random();
             for (int i = 0; i < numberOfElements; i++) {
-                int elem = random.nextInt();
+                int elem =(int) Math.random()*Integer.MAX_VALUE;
                 file.write(elem + "\n");
             }
         } catch (IOException e) {
@@ -24,7 +23,7 @@ public class TestGenerator {
             file.write(numberOfElements + "\n");
             Random random = new Random();
             for (int i = 0; i < numberOfElements; i++) {
-                int elem = (int) (random.nextGaussian()*Integer.MAX_VALUE);
+                int elem =Math.abs ((int) (random.nextGaussian()*Integer.MAX_VALUE));
                 file.write(elem + "\n");
             }
         } catch (IOException e) {
