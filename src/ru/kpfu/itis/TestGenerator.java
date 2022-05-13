3fb019b56c87b3a94146055189163ad3a0c7e295
Елизавета Бельская <nefrf@mail.ru>
2022-05-13 10:20:48 +0300

    Tests updated

--- src/ru/kpfu/itis/TestGenerator.java
+++ src/ru/kpfu/itis/TestGenerator.java
@@ -2,16 +2,30 @@ package ru.kpfu.itis;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Random;
+import java.util.Set;
+
 import org.uncommons.maths.random.*;
 
 public class TestGenerator {
-    public void generateUniformlyDistributedTest(String path, int numberOfElements) {
+
+    public void generateTest(String path, int numberOfElements) {
         try (FileWriter file = new FileWriter(path)) {
             file.write(numberOfElements + "\n");
-            for (int i = 0; i < numberOfElements; i++) {
-                int elem =(int) (Math.random()*Integer.MAX_VALUE);
-                file.write(elem + "\n");
+            Set<Integer> set = new HashSet<>(numberOfElements);
+            for (int i = 1; i <= numberOfElements; i++) {
+                set.add(i);
+            }
+
+            int randomIndex = 1 + (int)(Math.random()*numberOfElements);
+            while (!set.isEmpty()) {
+                if (set.contains(randomIndex)) {
+                    file.write(randomIndex + "\n");
+                    set.remove(randomIndex);
+                }
+                randomIndex = 1 + (int)(Math.random()*numberOfElements);
             }
         } catch (IOException e) {
             e.printStackTrace();
