2b1769d827eb40a7ebee31816c74bc40dc45b823
Odenezhkina <66617436+Odenezhkina@users.noreply.github.com>
2022-05-13 02:17:33 -0700

    Measurements: done

--- src/ru/kpfu/itis/Main.java
+++ src/ru/kpfu/itis/Main.java
@@ -1,7 +1,11 @@
 package ru.kpfu.itis;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -28,6 +32,62 @@ public class Main {
         unsorted = bs.bucketSort(unsorted);
         array = qs.quickSort(array, 0, array.length - 1);
         System.out.println("Sorted array after quick sort- " + Arrays.toString(array));
-        System.out.println("Sorted array after bucket sort- " +  Arrays.toString(unsorted.toArray()));
+        System.out.println("Sorted array after bucket sort- " + Arrays.toString(unsorted.toArray()));
+
+
+        String path = "/Users/olga/IdeaProjects/semester-project-bucket-sort/test/UniformlyDistributedTest1";
+//        TestGenerator generator = new TestGenerator();
+//        generator.generateTest(path, 10000);
+        for (int i = 1; i < 21; i++) {
+            String link = path + i;
+            getAverageTime(link);
+        }
+    }
+
+    private static void getAverageTime(String linkToTestFile) {
+        int[] elems;
+        try (Scanner scanner = new Scanner(new FileReader(linkToTestFile))) {
+            elems = new int[scanner.nextInt()];
+            for (int i = 0; i < elems.length; i++) {
+                elems[i] = scanner.nextInt();
+            }
+            double sumTimeTestQuickSort = 0;
+            double sumTimeTestBucketSort = 0;
+            double num = 100.0;
+
+            for (int j = 0; j < num; j++) {
+                sumTimeTestQuickSort += testQuickSort(elems);
+                sumTimeTestBucketSort += testBucketSort(elems);
+            }
+            System.out.println(elems.length);
+            System.out.printf("Average time for QuickSort: %.2f nanoseconds\n", (sumTimeTestQuickSort / num));
+            System.out.printf("Average time for BucketSort: %.2f nanoseconds", (sumTimeTestBucketSort / num));
+            System.out.println("\n");
+        } catch (FileNotFoundException ex) {
+            System.err.println(ex.getMessage());
+        } catch (NullPointerException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+
+    private static long testQuickSort(int[] elems) {
+        QuickSort qs = new QuickSort();
+        long start = System.nanoTime();
+        qs.quickSort(elems, 0, elems.length - 1);
+        return System.nanoTime() - start;
+    }
+
+    private static long testBucketSort(int[] elems) {
+        List<Integer> listOfElems = new ArrayList<>();
+        for (int i = 0; i < elems.length; i++) {
+            listOfElems.add(elems[i]);
+        }
+        BucketSort bs = new BucketSort();
+        long start = System.nanoTime();
+        bs.bucketSort(listOfElems);
+        long timeWorkCode = System.nanoTime() - start;
+        return timeWorkCode;
     }
 }
+
+
